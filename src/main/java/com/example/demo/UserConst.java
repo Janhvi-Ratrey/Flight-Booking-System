fc745a78bfdf9acbae7b896f91f888839c725f97
Janhvi-Ratrey <60838184+Janhvi-Ratrey@users.noreply.github.com>
2020-03-31 20:48:59 +0530

    Update UserConst.java

--- src/main/java/com/example/demo/UserConst.java
+++ src/main/java/com/example/demo/UserConst.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public interface UserConst {
 	public static final String BEARER_NAME = "Bearer";
-	public static final String ISSUER_NAME = "jayant";
+	public static final String ISSUER_NAME = "janhvi";
 	public static final String SECRET_KEY = "top_secret_key";
 }
