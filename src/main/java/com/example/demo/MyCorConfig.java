d3056dfad4be027b64f302983030e2cc90ca8b14
Janhvi-Ratrey <60838184+Janhvi-Ratrey@users.noreply.github.com>
2020-03-31 20:51:14 +0530

    Update MyCorConfig.java

--- src/main/java/com/example/demo/MyCorConfig.java
+++ src/main/java/com/example/demo/MyCorConfig.java
@@ -14,7 +14,7 @@ public class MyCorConfig implements WebMvcConfigurer
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500");
+
                 registry.addMapping("/**").allowedOrigins("*");
             }
         };
